16400df13d2193dc43b8cc88e5dc6fc1b0c3db3f
dherrada <dylan.herrada@adafruit.com>
2020-07-17 09:44:15 -0400

    Cleaned up example

--- examples/dht_simpletest.py
+++ examples/dht_simpletest.py
@@ -3,7 +3,12 @@ import board
 import adafruit_dht
 
 # Initial the dht device, with data pin connected to:
-dhtDevice = adafruit_dht.DHT22(board.D18, use_pulseio=False)
+dhtDevice = adafruit_dht.DHT22(board.D18)
+
+# you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
+# This may be necessary on a Linux single board computer like the Raspberry Pi,
+# but it will not work in CircuitPython.
+#dhtDevice = adafruit_dht.DHT22(board.D18, use_pulseio=False)
 
 while True:
     try:
