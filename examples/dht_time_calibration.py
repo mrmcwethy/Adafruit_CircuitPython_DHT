378d6c219c05a46590fa48b2823234b2f0194634
yeyeto2788 <juanernestobiondi@gmail.com>
2021-03-19 14:45:05 +0100

    Update dht_time_calibration.py
    Fix issues on linting.

--- examples/dht_time_calibration.py
+++ examples/dht_time_calibration.py
@@ -8,7 +8,7 @@ import board
 
 import adafruit_dht
 
-pin_to_use = 'PG6'
+pin_to_use = "PG6"
 
 # Maximum number of tries per timing
 max_retries_per_time = 10
@@ -22,17 +22,16 @@ time_increment = 100
 # Variable to store all reads on a try
 reads = {}
 
-print(
-    "\nInitializing test.\n",
-    f"Total tries per trig_wait {max_retries_per_time}"
-)
+print("\nInitializing test.\n", f"Total tries per trig_wait {max_retries_per_time}")
 
 for milliseconds in range(min_time, max_time, time_increment):
     # Instantiate the DHT11 object.
     dhtDevice = adafruit_dht.DHT11(pin=getattr(board, pin_to_use))
     # Change the default wait time for triggering the read.
+    # pylint: disable=W0212
     dhtDevice._trig_wait = milliseconds
 
+    # pylint: disable=W0212
     print(f"Using 'trig_wait' of {dhtDevice._trig_wait}")
     # Reset the read count for next loop
     reads_count = 0
@@ -40,19 +39,14 @@ for milliseconds in range(min_time, max_time, time_increment):
     # Create the key on the reads dictionary with the milliseconds used on
     # this try.
     if milliseconds not in reads:
-        reads[milliseconds] = {
-            "total_reads": 0
-        }
+        reads[milliseconds] = {"total_reads": 0}
 
     for try_number in range(0, max_retries_per_time):
         try:
             # Read temperature and humidity
             temperature = dhtDevice.temperature
             humidity = dhtDevice.humidity
-            read_values = {
-                "temperature": temperature,
-                "humidity": humidity
-            }
+            read_values = {"temperature": temperature, "humidity": humidity}
 
             if try_number not in reads[milliseconds]:
                 reads[milliseconds][try_number] = read_values
@@ -68,17 +62,14 @@ for milliseconds in range(min_time, max_time, time_increment):
     print(f"Total read(s): {reads[milliseconds]['total_reads']}\n")
     dhtDevice.exit()
 
-
 # Gather the highest read numbers from all reads done.
-best_result = max(
-    [
-        reads[milliseconds]["total_reads"] for milliseconds in reads
-    ]
-)
+best_result = max([reads[milliseconds]["total_reads"] for milliseconds in reads])
 
 # Gather best time(s) in milliseconds where we got more reads
 best_times = [
-    milliseconds for milliseconds in reads if reads[milliseconds]['total_reads'] == best_result
+    milliseconds
+    for milliseconds in reads
+    if reads[milliseconds]["total_reads"] == best_result
 ]
 print(
     f"Maximum reads: {best_result}  out of {max_retries_per_time} with the "
@@ -87,4 +78,5 @@ print(
 
 # change the value on the line below to see all reads performed.
 print_all = False
-print(json.dumps(reads)) if print_all else None
+if print_all:
+    print(json.dumps(reads))
