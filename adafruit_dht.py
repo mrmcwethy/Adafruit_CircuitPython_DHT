b092f90e7a741e9dec0821aaca93c6d7fa05e414
xgqfrms <xgqfrms@outlook.com>
2023-05-16 23:24:55 +0800

    fix: the issues 90
    https://github.com/adafruit/Adafruit_CircuitPython_DHT/issues/90

--- adafruit_dht.py
+++ adafruit_dht.py
@@ -89,7 +89,6 @@ class DHTBase:
     def exit(self) -> None:
         """Cleans up the PulseIn process. Must be called explicitly"""
         if self._use_pulseio:
-            print("De-initializing self.pulse_in")
             self.pulse_in.deinit()
 
     def _pulses_to_binary(self, pulses: array.array, start: int, stop: int) -> int:
